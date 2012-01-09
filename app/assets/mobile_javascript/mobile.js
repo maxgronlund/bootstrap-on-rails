b380a2d91d61f18ab370a73081e8a1a339337c42
Synthmax <max@synthmax.dk>
2012-01-09 02:24:55 +0100

    soundmanager2 conflict with jquery mobile

--- app/assets/mobile_javascript/mobile.js
+++ app/assets/mobile_javascript/mobile.js
@@ -6,10 +6,14 @@
 //
 //= require jquery
 //= require jquery_ujs
-// require_tree ../../../vendor/assets/javascripts
+//= require_tree ../../../vendor/assets/javascripts
 // require_directory .
 //= require rails.validations
 
 //= require jqm-docs.js
 //= require jquery.mobile-1.0.min.js
 //= require jquery.mobile.themeswitcher.js
+
+
+//= require 01_soundmanager2-nodebug-jsmin
+//= require 02_inlineplayer
